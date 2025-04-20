357a0f55b8fd05cee8248045e62bdafa4b35f2f8
oxishtiak <22101044@uap-bd.edu.com>
2025-04-20 19:07:20 +0600

    home function defined

---
+++ childcare/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.decorators import login_required
+import uuid
+from django.utils.timezone import now
+from datetime import timedelta
+from django.db.models import Count, Sum
+
+# Create your views here.
+def Home(request):
+    return render(request, 'home.html')
